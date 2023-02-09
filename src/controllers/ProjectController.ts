c37b544837e00309bd30e9f417b113e8bea45bbe
laidaoduy <laidd@hybrid-technologies.vn>
2023-02-09 14:56:44 +0700

    Add route get project

--- src/controllers/ProjectController.ts
+++ src/controllers/ProjectController.ts
@@ -5,6 +5,7 @@ import Log from '../utils/Log';
 import { ProjectService } from '../services/ProjectService';
 import { Project } from '../bo/entities/Project';
 import { ExistedException } from '../exceptions/ExistedException';
+import { NotFoundException } from '../exceptions/NotFoundException';
 
 @Service()
 @Controller('api/project')
@@ -38,6 +39,25 @@ export class ProjectController {
       const newProject: Project = await this.projectService.store(projectBody);
 
       res.status(200).json({ data: newProject });
+    } catch (e) {
+      next(e);
+    }
+  }
+
+  @Get(':projectId')
+  public async getProject(req: Request, res: Response, next: NextFunction): Promise<void> {
+    Log.info(this.className, 'listProject', 'RQ', { req: req });
+
+    try {
+      const projectId: number = Number.parseInt(req.params.projectId);
+      const project: Project = await this.projectService.findById(projectId, {
+        relations: ['sections']
+      });
+      if (!project) {
+        throw new NotFoundException(projectId);
+      }
+
+      res.status(200).json({ data: project });
     } catch (e) {
       next(e);
     }
